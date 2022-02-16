81aaea5139e7a9cbfffee1300e72dfce81ccc4fc
Chlodion Gossart <chlodiongossart@gmail.com>
2022-02-17 00:27:31 +0100

    added url product

--- front/js/product.js
+++ front/js/product.js
@@ -4,9 +4,10 @@ console.log (queryString_url_id);
 
 /*
 //Récupération de l'ID du produit (Méthode Slice)
-const idProduit = queryString_url_id.slice(4);
-console.log(idProduit);
+const productId = queryString_url_id.slice(4);
+console.log(productId);
 */
+
 
 //Récupération de l'ID du produit (Méthode URLSearchParams)
 const urlSearchParams = new URLSearchParams(queryString_url_id);
@@ -14,3 +15,31 @@ console.log(urlSearchParams);
 
 const productId = urlSearchParams.get("id");
 console.log(productId);
+
+//Modification de l'URL de l'API, en fonction de l'ID du produit
+const newUrl = `http://localhost:3000/api/products/${productId}`;
+console.log(newUrl);
+
+//Récupération des données du produit sur l'API
+fetch(newUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    const product = data;
+    //selectedProduct(product);
+    console.log(product);
+
+    //Intégration des données du produit
+    function selectedProduct (product) {
+      let productImg = document.getElementByClassName("item_img");
+      productImg.innerHTML += 
+      `<img src="${product.imageUrl}" alt="${product.altTxt}"></img>`;
+    }
+  })
+
+
+
+/*
+Récupérer l'ID du produit
+Interroger l'API pour accéder aux données
+Modifier le DOM avec les datas du produit
+*/
--- front/js/script.js
+++ front/js/script.js
@@ -1,13 +1,13 @@
 //Récupération des données de l'API
 fetch("http://localhost:3000/api/products/")
-  .then(function(data) {
-    if (data.ok) {
-      return data.json();
+  .then(function(response) { //Version courte : .then((response) => response.json())
+    if (response.ok) {
+      return response.json();
     }
-  })
+  }) 
   //Création d'une liste des produits à partir des données de l'API
   .then(function(jsonListProduct) {
-    //Intégration des produits dans la page d'accueil
+    //Intégration des différents produits dans la page d'accueil
     for(let product of jsonListProduct) {
       let i = 0; i < product.length; i++;
       document.getElementById("items").innerHTML += `<a href="./product.html?id=${product._id}">
@@ -19,7 +19,7 @@ fetch("http://localhost:3000/api/products/")
                                                     </a>`
     }
   })
-  //En cas d'erreur de récupération des données de l'API
+  //En cas d'échec de récupération des données de l'API
   .catch(function(error) {
     console.log(error);
   });
@@ -33,24 +33,3 @@ Créer une liste des produits au format JSON
 Modifier le DOM
 Intégrer chaque produit avec une boucle FOR
 */
-
-
-
-  /* OLD CODE
-  .then(function(product) {
-    for (let i = 0; i < product.length; i++) {
-      document.getElementById("items").innerHTML += `<a href="${product._id}">
-                                                      <article>
-                                                        <img src="http://localhost:3000/api/products/${product.imageUrl}" alt="${product.altTxt}">
-                                                        <h3 class="productName">${product.name}</h3>
-                                                        <p class="productDescription">${product.description}</p>
-                                                      </article>
-                                                    </a>`
-    }
-    console.log(product);
-  })
-  .catch(function(error) {
-    console.log(error)
-    // Une erreur est survenue
-  });
-  */
