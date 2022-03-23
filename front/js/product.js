ac68341d1e563460feb6632a281600b5da101660
Chlodion Gossart <chlodiongossart@gmail.com>
2022-03-23 16:06:41 +0100

    fixed product features

--- front/js/product.js
+++ front/js/product.js
@@ -1,146 +1,112 @@
-//Récupération de la chaîne de requête dans l'URL
-const queryString_url_id = window.location.search;
-console.log (queryString_url_id);
+//Récupération de l'ID du produit
+const getProductId = () => {
+  return new URL(location.href).searchParams.get("id");
+};
+const productId = getProductId();
 
-/*
-//Récupération de l'ID du produit (Méthode Slice)
-const productId = queryString_url_id.slice(4);
-console.log(productId);
-*/
+fetch(`http://localhost:3000/api/products/${productId}`)
+  .then((response) => {
+    return response.json();
+  })
 
-//Récupération de l'ID du produit (Méthode URLSearchParams)
-const urlSearchParams = new URLSearchParams(queryString_url_id);
-console.log(urlSearchParams);
+  .then((product) => {
+    selectedProduct(product);
+    registredProduct(product);
+  })
+  .catch((error) => {
+    alert(error);
+  });
 
-const productId = urlSearchParams.get("id");
-console.log(productId);
+// Sélection de l'ID colors
+const selectedColor = document.querySelector("#colors");
 
-//Modification de l'URL de l'API, en fonction de l'ID du produit
-const url = `http://localhost:3000/api/products/${productId}`;
-console.log(url);
+// Sélection de l'ID quantity
+const selectedQuantity = document.querySelector("#quantity");
 
-//Constantes et fonctions nécessaires à l'intégration de la photo de produit
-const div = document.getElementsByClassName("item__img");
-console.log(div);
+// Sélection du bouton Ajouter au panier
+const button = document.querySelector("#addToCart");
 
-function createNode(element) {
-  return document.createElement(element);
-}
+// Fonction qui récupère les données de la promesse .then(product) pour injecter les valeurs dans le fichier Html
+let selectedProduct = (product) => {
 
-function append(parent, el) {
-  return parent.appendChild(el);
-}
+  // Intégration des données du produit sélectionné dans la page HTML
+  document.querySelector("head > title").textContent = product.name;
+  document.querySelector(".item__img")
+  .innerHTML += `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+  document.querySelector("#title").textContent += product.name;
+  document.querySelector("#price").textContent += product.price;
+  document.querySelector("#description").textContent += product.description;
 
-//Récupération des données du produit sur l'API
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    const product = data;
-    
-    //Intégration de la photo du produit
-    let img = createNode('img');
-    img.src = product.imageUrl;
-    img.alt = product.altTxt;
-    console.log(img);
-    append(div[0], img);
+  // Boucle intégrant les différentes couleurs du produit dans le HTML
+  for (color of product.colors) {
+    let option = document.createElement("option");
+    option.innerHTML = `${color}`;
+    option.value = `${color}`;
+    selectedColor.appendChild(option);
+  }
+};
 
-    //Intégration du nom du produit
-    let productName = document.getElementById("title");
-    productName.innerHTML += `<h1 id="title">${product.name}</h1>`;
-    console.log(productName);
+// Fonction qui enregistre dans un objet les options de l'utilisateur au click sur le bouton ajouter au panier
+let registredProduct = (product) => {
 
-    //Intégration du prix
-    let productPrice = document.getElementById("price");
-    productPrice.innerHTML += `<span id="price">${product.price}</span>`;
-    console.log(productPrice);
+  // Écoute de l'évènement click sur le bouton ajouter
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
 
-    //Intégration de la description
-    let productDescription = document.getElementById("description");
-    productDescription.innerHTML += `<p id="description">${product.description}</p>`;
-    console.log(productDescription);
+    if (selectedColor.value == false) {
+      confirm("Veuillez sélectionner une couleur");
+    } else if (selectedQuantity.value == 0) {
+      confirm("Veuillez sélectionner le nombre d'articles souhaités");
+    } else {
+      alert("Votre article a bien été ajouté au panier");
 
-    //Intégration de la couleur
-    product.colors.forEach(productColor => {
-      option = document.createElement('option');
-      option.value = productColor;
-      option.innerHTML = productColor;
-      colors.appendChild(option);
-      console.log(productColor);
-    })
-
-    /*
-    //Contenu de la page produit
-    Récupérer l'ID du produit
-    Interroger l'API pour accéder aux données
-    Modifier le DOM avec les datas du produit
-    */
-  
-    //Ajout du produit dans le panier
-    let button = document.getElementById("addToCart"); //Définir le bouton
-
-    button.addEventListener("click", function productInCart() { //La fonction permettant d'ajouter le produit sélectionné au panier
-      let selectedId = productId;
-      let selectedName = document.getElementById("title").textContent;
-      let selectedQuantity = document.getElementById("quantity").value;
-      let selectedColor = document.getElementById("colors").value;
-      const selectedProduct = {
-        "id": selectedId,
-        "name": selectedName,
-        "quantity": selectedQuantity,
-        "color": selectedColor,
+      // Récupération des informations du produit sélectionné
+      let selectedProduct = {
+        id: product._id,
+        name: product.name,
+        img: product.imageUrl,
+        altTxt: product.altTxt,
+        description: product.description,
+        color: selectedColor.value,
+        quantity: parseInt(selectedQuantity.value, 10),
+        price: product.price,
+        totalPrice: product.price * parseInt(selectedQuantity.value, 10),
       };
       console.log(selectedProduct);
 
-      //Conditions d'ajout au panier
-      if (selectedQuantity == 0) {
-        alert("Veuillez saisir le nombre de canapés.");
-      } else if (selectedColor == "") {
-        alert("Veuillez choisir une couleur.");
-      } else {
-        //Ajoute le produit au localStorage
-        localStorage.setItem("allCouches", JSON.stringify(selectedProduct));
-        //window.location.assign("cart.html"); //Redirection vers la page Panier
-        function otherProductInCart() { // Ajouter un autre produit au localStorage
-          let existingCart = JSON.parse(localStorage.getItem("allCouches")); //Pour vérifier si le produit est déjà dans le panier
-          let entryId = productId;
-          let entryName = document.getElementById("title").textContent;
-          let entryQuantity = document.getElementById("quantity").value;
-          let entryColor = document.getElementById("colors").value;
-          const entryProduct = {
-            "id": entryId,
-            "name": entryName,
-            "quantity": entryQuantity,
-            "color": entryColor,
-          };
-          if (existingCart.entryId == entryProduct.id) {
-            otherProductInCart.then(() => {
-              if (existingCart.entryColor == entryProduct.color) {
-                addToCart.then(() => { // Si la couleur est identique, incrémente seulement la quantité
-                  existingCart.entryQuantity += entryProduct.quantity;
-                  localStorage.setItem("entryCouches", JSON.stringify(entryProduct));
-                  })
-              } else {
-              // Si la couleur est différente, ajoute le produit au localStorage
-              existingCart.push(entryProduct);
-              localStorage.setItem("allCouches", JSON.stringify(existingCart))
-              }
-            })
-            } else {
-              //Si l'ID est différent, ajoute le produit au localStorage
-              existingCart.push(entryProduct);
-              localStorage.setItem("allCouches", JSON.stringify(existingCart));
-              otherProductInCart();
-            }
-          }
-        }
-      })
-    })
+      /* Gestion du Local Storage */
 
-/*
-// Ajouter les produits au panier
-Intégrer le bouton
-Définir les valeurs à récupérer dans le panier : id, quantité et couleur sélectionnée
-Cliquer sur le bouton pour ajouter au panier
-Produit ajouté au panier avec les bonnes informations
-Deux cas de figure à intégrer : Si le produit est déja dans le panier, s'il n'est pas dans le panier
-*/
+      //Récupération des données du Local Storage
+      let existingCart = JSON.parse(localStorage.getItem("cart"));
+
+      // Si le Local Storage existe
+      if (existingCart) {
+        console.log("Il y a déjà un produit dans le panier, on compare les données");
+        // On recherche avec la méthode find() si l'id et la couleur d'un article sont déjà présents
+        let item = existingCart.find(
+          (item) =>
+            item.id == selectedProduct.id && item.color == selectedProduct.color
+        );
+        // Si oui, on incrémente la nouvelle quantité et la mise à jour du prix total de l'article
+        if (item) {
+          item.quantity = item.quantity + selectedProduct.quantity;
+          item.totalPrice += item.price * selectedProduct.quantity;
+          localStorage.setItem("cart", JSON.stringify(existingCart));
+          console.log("Quantité supplémentaire dans le panier.");
+          return;
+        }
+        // Si non, alors on push le nouvel article sélectionné
+        existingCart.push(selectedProduct);
+        localStorage.setItem("cart", JSON.stringify(existingCart));
+        console.log("Le produit a été ajouté au panier");
+
+      } else {
+        //  Sinon création d'un tableau dans le lequel on push l'objet "selectedProduct"
+        let createLocalStorage = [];
+        createLocalStorage.push(selectedProduct);
+        localStorage.setItem("cart", JSON.stringify(createLocalStorage));
+        console.log("Le panier est vide, on ajoute le premier produit");
+      }
+    }
+  });
+};
